7eb70a3c20757a786a5c811c9eb4119dd1190ab1
Keldon Lopez <keldon.lopz@gmail.com>
2023-07-06 12:20:49 -0600

    finished part 4 step 1

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,55 @@
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { SECRET } = process.env;
+const { User } = require("./models/user");
+
+const createToken = (username, id) => {
+  let data = {
+    username: username,
+    id: id,
+  };
+
+  return jwt.sign(data, SECRET, { expiresIn: 60 * 60 * 48 });
+};
+
 module.exports = {
-    register: (req, res) => {
-        console.log('register')
-    },
-    login: (req, res) =>{
-        console.log('login')
-    },
-    logout: (req, res) => {
-        console.log('logout')
+  register: async (req, res) => {
+    try {
+      let { username, password } = req.body;
+
+      let foundUser = await User.findOne({ where: { username: username } });
+      if (foundUser) {
+        res.status(400).send("cannot create user");
+      } else {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        let newUser = await User.create({
+          username: username,
+          hashedPass: hash,
+        });
+        const token = createToken(
+          newUser.dataValues.username,
+          newUser.dataValues.id
+        );
+        console.log("TOKEN", token);
+        const exp = Date.now() + 1000 * 60 * 60 * 48;
+        res.status(200).send({
+          username: newUser.dataValues.username,
+          userId: newUser.dataValues.id,
+          token,
+          exp,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      res.sendStatus(400);
     }
-}
+  },
+  login: (req, res) => {
+    console.log("login");
+  },
+  logout: (req, res) => {
+    console.log("logout");
+  },
+};
